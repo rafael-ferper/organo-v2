8dc7c50254b406c4ea9393039d312d491e6ca36b
Rafael Fernandes Pereira <rtio.fernandesp@gmail.com>
2024-02-07 04:40:40 -0300

    feat: adicionar e deletar times, favoritar membros

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,42 @@
 import './colaborador.css'
+import {AiFillCloseCircle} from "react-icons/ai";
+import {CiHeart} from "react-icons/ci";
+import {FaHeart} from "react-icons/fa";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({colaborador, corDeFundo, aoDeletar, aoFavoritar}) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
+    return (
+        <div className="colaborador">
+            <AiFillCloseCircle
+                size={25}
+                className='deletar'
+                onClick={() => aoDeletar(colaborador.id)}/>
+            <div
+                className="cabecalho"
+                style={{
+                backgroundColor: corDeFundo
+            }}>
+                <img src={colaborador.imagem} alt={colaborador.nome}/>
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+                <div className='favoritar'>
+                    {colaborador.favorito
+                        ? <FaHeart {...propsFavorito} color='#ff0000'/>
+                        : <CiHeart {...propsFavorito}/>}
+                </div>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+    )
 }
 
 export default Colaborador
